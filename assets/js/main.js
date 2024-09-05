5218654140e9d5ad407f38304ac39b42b1f096db
Caeilanlightwood77 <sinahanbilly@outlook.com>
2024-09-06 00:27:42 +0800

    My Portfolio w Tailwindcss

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+import Typed from 'typed.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var options = {
+    strings: ["Back-End Developer", "Software Developer", "Data Analyst", "Cryptanalyst"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+  };
+
+  var typed = new Typed('.typed', options);
+});
